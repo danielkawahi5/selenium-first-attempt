5fd1acfe5ae77225ac4cae1bef459a76d12e093a
gofer92 <daniel.kawa.hi5@gmail.com>
2019-08-05 10:02:58 +0200

    commit one

--- src/main/java/SeleniumD.java
+++ src/main/java/SeleniumD.java
@@ -2,9 +2,19 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SeleniumD {
 
+
+    /*public static void main(String[] args) {
+        System.setProperty("webdriver.firefox.marionette","C:\\Users\\danie\\drivers\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://demo.guru99.com/selenium/deprecated.html");
+        driver.switchTo().frame("classFrame");
+        driver.findElement(By.linkText("Deprecated")).click();
+        driver.close();
+    }*/
 
 
     public static void main(String args[]){
